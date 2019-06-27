a8076b647fadf263bcd33c1315bb1a22fc371c21
yao.xu <yao.xu@saiwangyun.com>
2019-06-27 17:50:21 +0800

    shiro

---
+++ src/main/java/com/xuyao/springsession/config/CustomCredentialsMatcher.java
@@ -0,0 +1,17 @@
+package com.xuyao.springsession.config;
+
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.credential.HashedCredentialsMatcher;
+
+public class CustomCredentialsMatcher extends HashedCredentialsMatcher {
+
+    @Override
+    public boolean doCredentialsMatch(AuthenticationToken token, AuthenticationInfo info) {
+        String username = (String) token.getPrincipal();
+
+        boolean match = super.doCredentialsMatch(token, info);
+
+        return match;
+    }
+}
