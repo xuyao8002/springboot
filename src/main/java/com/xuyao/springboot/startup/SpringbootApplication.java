fa852f476d100d05ad16bb0fa5683c66403d4fd6
xuyao <xuyao0@foxmail.com>
2020-04-05 10:52:58 +0800

    配置文件整理，部分配置独立到单独文件 单元测试指定配置文件 pom文件整理

--- src/main/java/com/xuyao/springboot/startup/SpringbootApplication.java
+++ src/main/java/com/xuyao/springboot/startup/SpringbootApplication.java
@@ -6,14 +6,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 @SpringBootApplication
 @ComponentScan("com.xuyao.springboot")
 @MapperScan("com.xuyao.springboot.dao")
 @EnableAsync
 public class SpringbootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootApplication.class, args);
+	public static void main(String[] args) throws IOException {
+		SpringApplication app = new SpringApplication(SpringbootApplication.class);
+		setOtherProperties(app);
+		app.run(args);
+	}
+
+	private static void setOtherProperties(SpringApplication app) throws IOException {
+		Properties properties = new Properties();
+		InputStream resourceStream = SpringbootApplication.class.getClassLoader().getResourceAsStream("other.properties");
+		properties.load(resourceStream);
+		app.setDefaultProperties(properties);
 	}
 
 }
--- src/test/java/com/xuyao/springboot/AsyncTaskTests.java
+++ src/test/java/com/xuyao/springboot/AsyncTaskTests.java
@@ -6,12 +6,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootApplication.class)
+@TestPropertySource("classpath:other.properties")
 public class AsyncTaskTests {
 
     @Autowired
