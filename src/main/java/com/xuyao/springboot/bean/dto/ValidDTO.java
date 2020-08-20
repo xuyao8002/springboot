2ee55ca5e9f1e458338f1fe25d8b1d570e6987d2
yao.xu <yao.xu@saiwangyun.com>
2020-08-20 20:28:25 +0800

    DTO日期格式化

--- src/main/java/com/xuyao/springboot/bean/dto/ValidDTO.java
+++ src/main/java/com/xuyao/springboot/bean/dto/ValidDTO.java
@@ -3,11 +3,15 @@ package com.xuyao.springboot.bean.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.validation.Valid;
+import java.time.LocalDateTime;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.groups.Default;
-import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Getter
 @Setter
@@ -21,12 +25,10 @@ public class ValidDTO {
 
     @NotBlank(message = "name必填", groups = {Insert.class, Update.class})
     private String name;
-
-    //@Valid嵌套校验
-    @Valid
-    @NotNull(message = "others不能为空")
-    private List<NestedDTO> others;
-
+    //get请求日期格式化
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    // @DateTimeFormat(pattern ="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime date;
     public interface Insert extends Default {};
     public interface Update extends Default {};
 
