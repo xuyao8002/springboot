75ef67b6b74e99f2d334eb243fcb039f6ebf0344
xuyao <xuyao0@foxmail.com>
2020-08-27 21:59:18 +0800

    redis基数统计HyperLogLog

--- src/test/java/com/xuyao/springboot/RedisTests.java
+++ src/test/java/com/xuyao/springboot/RedisTests.java
@@ -6,13 +6,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.concurrent.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringbootApplication.class)
-public class RedisTemplateTests {
+@TestPropertySource("classpath:other.properties")
+public class RedisTests {
 
 
 	@Autowired
@@ -34,5 +39,22 @@ public class RedisTemplateTests {
         System.out.println("end");
 
 	}
+
+	@Test
+    public void hyperLogLog(){
+	    int i = 1000_0000;
+        String hello = "hello";
+        int x = 0;
+        for (int i1 = 0; i1 < 10000; i1++) {
+            String[] vals = new String[1000];
+            for(int j = 0; j < 1000; j++){
+                vals[j] = String.valueOf(x++);
+            }
+            redisTemplate.opsForHyperLogLog().add(hello, vals);
+        }
+        System.out.println("add " + i);
+        System.out.println(redisTemplate.opsForHyperLogLog().size(hello));
+    }
+
 
 }
